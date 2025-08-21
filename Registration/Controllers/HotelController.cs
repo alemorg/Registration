9b306f97ea587bfd4d2e6054cadd83fdbecfc701
Alemorg <anserku@yandex.ru>
2025-08-21 09:21:17 +0300

    Add Method CorrectRoom

--- Registration/Controllers/HotelController.cs
+++ Registration/Controllers/HotelController.cs
@@ -7,7 +7,6 @@ namespace Registration.Controllers
 {
     public class HotelController : Controller
     {
-
         [HttpGet]
         public IActionResult CreateRoom()
         {
@@ -32,7 +31,7 @@ namespace Registration.Controllers
             return View(room);
         }
 
-        public IActionResult CompleteCreateRoom (Room room)
+        public IActionResult CompleteCreateRoom(Room room)
         {
             return View(room);
         }
@@ -54,33 +53,77 @@ namespace Registration.Controllers
             }
         }
 
-        [HttpGet("{id}")]
+        //[HttpGet("{id}")]
+        [HttpGet]
         public IActionResult DeleteRoom(int id)
         {
             Room room = new Room();
-            if (ModelState.IsValid)
+            using (BookedDB dB = new BookedDB())
             {
-                using (BookedDB dB = new BookedDB())
-                {
 
-                    try
-                    {
-                        room = dB.RoomInfo.FirstOrDefault(x => x.Id == id);
-                        dB.RoomInfo.Remove(room);
-                        dB.SaveChanges();
-                    }
-                    catch (Exception ex) 
-                    {
-                        Console.WriteLine(ex.Message);
-                        return View(NotFound());
-                    }
+                try
+                {
+                    room = dB.RoomInfo.FirstOrDefault(x => x.Id == id);
+                    dB.RoomInfo.Remove(room);
+                    dB.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return View(NotFound());
                 }
             }
+
             return View("CompleteDeleteRoom", room);
         }
 
-        public IActionResult CompleteDeleteRoom (Room room)
+        public IActionResult CompleteDeleteRoom(Room room)
         {
+            return View(room);
+        }
+
+        [HttpGet]
+        public IActionResult CorrectRoom (int id)
+        {
+            if (id <= 0)
+                return NotFound();
+            Room room = new Room();
+            using BookedDB dB = new BookedDB();
+            {
+                room = dB.RoomInfo.FirstOrDefault( x => x.Id == id);
+                return View(room);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CorrectRoom(int id, Room room)
+        {
+            if (id <= 0)
+                NotFound();
+            
+            if (ModelState.IsValid)
+            {
+                if (room.Square > 0 && room.MaximumGuests > 0)
+                {
+                    using BookedDB dB = new BookedDB();
+                    {
+                        try
+                        {
+                            Room roomdb = dB.RoomInfo.FirstOrDefault(x => x.Id == id);
+                            roomdb.Square = room.Square;
+                            roomdb.MaximumGuests = room.MaximumGuests;
+                            dB.RoomInfo.Update(roomdb);
+                            dB.SaveChanges();
+
+                            return View("CompleteCorrectRoom", room);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error {ex.Message}");
+                        }
+                    }
+                }
+            }
             return View(room);
         }
     }
